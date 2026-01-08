7beec962bcb790f0ae3e32e6027dae74f6e05822
KameraSaki <adjosdaniel108@gmail.com>
2026-01-08 20:27:45 +0100

    update

---
+++ src/main.py
@@ -0,0 +1,15 @@
+
+
+
+extensions = [
+    ".txt", ".pdf", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx",
+    ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".webp",
+    ".mp4", ".mov", ".avi", ".mkv",
+    ".mp3", ".wav", ".flac",
+    ".zip", ".rar", ".7z", ".tar", ".gz",
+    ".py", ".js", ".html", ".css", ".json", ".xml", ".md",
+    ".exe", ".msi",
+    ".ttf", ".otf",
+    ".csv",
+    ".epub", ".mobi",
+]
